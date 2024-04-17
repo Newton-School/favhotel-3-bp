95df2a517f13c8fdd0a28c664076558f65565a6c
trojan1771 <bt20cse141@iiitn.ac.in>
2024-04-17 16:58:15 +0530

    final

---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { hotelData } from '../utils/hotelData'
+import Card from './Card'
+
+const Main = () => {
+  return <div className='main'></div>
+}
+
+export default Main
